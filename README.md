95db056390e11d6f2114484a3b7793be3f78f060
Dmytro <74217403+DmytroCrawford@users.noreply.github.com>
2024-11-09 13:48:20 -0500

    rewrite

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# vscode-mamma README
+# vscode-momma README
 
-This is the README for your extension "vscode-mamma". After writing up a brief description, we recommend including the following sections.
+This is the README for your extension "vscode-momma". After writing up a brief description, we recommend including the following sections.
 
 ## Features
 
--- src/extension.ts
+++ src/extension.ts
@@ -8,15 +8,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-mamma" is now active!');
+	console.log('Congratulations, your extension "vscode-momma" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-mamma.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('vscode-momma.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from VsCode Mamma!');
+		vscode.window.showInformationMessage('Hello World from VsCode Momma!');
 	});
 
 	context.subscriptions.push(disposable);
